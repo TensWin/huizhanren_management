bf0c272a4adda6d73b1b75bc3833b1cb565c6bc3
mizne <w20054319@126.com>
2017-12-19 16:06:45 +0800

    try

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -14,7 +14,7 @@ export class DataService {
     fetchAudience():Observable<Audience[]> {
         return this.http
             .get(this.serverUrl + this.queryAudienceUrl)
-            .map(resp => (resp as APIResponse).result).do(a=>console.log(a))
+            .map(resp => (resp as APIResponse).result)
             .catch(this.handleError)
     }
     searchAudience(
